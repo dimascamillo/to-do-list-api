f408c8a6a6eb07ddb8e376dc0b28cb0776f3c357
Dimas Camillo <dimas.camillo@sonda.com>
2023-10-12 11:00:29 -0300

    Create new tables and Rout Create Users

--- prisma/migrations/20231012132148_create_tables/migration.sql
+++ prisma/migrations/20231012132148_create_tables/migration.sql
@@ -16,9 +16,13 @@ CREATE TABLE "tasks" (
     "status" BOOLEAN NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
 
     CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
